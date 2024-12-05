7d47a205153738c1a0c0b37454a936f6c87b3000
saalim-km <saalimkm@gmail.com>
2024-12-05 18:40:31 +0530

    removed all the comments

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,43 +1,43 @@
-//? Time Complexity - O(n^2)
-//? Space Complexity - O(1)
-// function bubbleSort(arr) {
-//     let swapped;
+// ? Time Complexity - O(n^2)
+// ? Space Complexity - O(1)
+function bubbleSort(arr) {
+    let swapped;
 
-//     do {
-//         swapped = false;
+    do {
+        swapped = false;
 
-//         for(let i=0;i<arr.length;i++) {
+        for(let i=0;i<arr.length;i++) {
 
-//             if(arr[i] > arr[i+1]){
-//                 [arr[i] , arr[i+1]] = [arr[i+1] , arr[i]];
-//                 swapped = true;
-//             }
-//         }
-//     } while (swapped);
+            if(arr[i] > arr[i+1]){
+                [arr[i] , arr[i+1]] = [arr[i+1] , arr[i]];
+                swapped = true;
+            }
+        }
+    } while (swapped);
 
-//     return arr;
-// }
+    return arr;
+}
 
-// console.log(bubbleSort([5,4,3,2,1]));
+console.log(bubbleSort([5,4,3,2,1]));
 
 
 // ? DESCENDING ORDER BUBBLE SORT
-// function bubbleSort(arr) {
-//     let swapped;
+function bubbleSort(arr) {
+    let swapped;
 
-//     do {
-//         swapped = false;
+    do {
+        swapped = false;
 
-//         for(let i=0;i<arr.length;i++) {
+        for(let i=0;i<arr.length;i++) {
 
-//             if(arr[i] < arr[i+1]){
-//                 [arr[i] , arr[i+1]] = [arr[i+1] , arr[i]];
-//                 swapped = true;
-//             }
-//         }
-//     } while (swapped);
+            if(arr[i] < arr[i+1]){
+                [arr[i] , arr[i+1]] = [arr[i+1] , arr[i]];
+                swapped = true;
+            }
+        }
+    } while (swapped);
 
-//     return arr;
-// }
+    return arr;
+}
 
-// console.log(bubbleSort([6,1,22,5,44,88]));
+console.log(bubbleSort([6,1,22,5,44,88]));
--- string/string.js
+++ string/string.js
@@ -2,125 +2,125 @@
 //?  USING TWO POINTER METHOD 
 //?  BOTH TIME && SPACE COMPLEXITY IS O(N)
 
-// function palindromCheck(word) {  
-//     word = word.toLowerCase();
-//     let start = 0;
-//     let end = word.length-1;
-//     while(start < end) {
-//         if(word[start] !== word[end]) {
-//             return false;
-//         }
-//         start++;
-//         end--;
-//     }
+function palindromCheck(word) {  
+    word = word.toLowerCase();
+    let start = 0;
+    let end = word.length-1;
+    while(start < end) {
+        if(word[start] !== word[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
 
-//     return true;
-// }
+    return true;
+}
 
-// console.log(palindromCheck('madam'));
+console.log(palindromCheck('madam'));
 
 
 //?  USING RECURSIVE METHOD
-// function palindromeRecrusrive(str,s,e) {
-//     str = str.toLowerCase();
-//     if(s >= e) {
-//         return true;
-//     }
+function palindromeRecrusrive(str,s,e) {
+    str = str.toLowerCase();
+    if(s >= e) {
+        return true;
+    }
 
-//     if(str[s] !== str[e]) {
-//         return false;
-//     }
+    if(str[s] !== str[e]) {
+        return false;
+    }
 
-//     return palindromeRecrusrive(str,s+1,e-1);
-// }
+    return palindromeRecrusrive(str,s+1,e-1);
+}
 
-// let str = 'malayalam';
-// let s=0;
-// let e=str.length-1;
-// console.log(palindromeRecrusrive(str,s,e));
+let str = 'malayalam';
+let s=0;
+let e=str.length-1;
+console.log(palindromeRecrusrive(str,s,e));
 
 
 
 //?  REVERSE STRING 
 //?  USING INBUILD REVERSE METHOD
 
-// function reverse(str) {
-//     return str.split('').reverse().join('');
-// }
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
 
-// console.log(reverse('hello'));
+console.log(reverse('hello'));
 
 
 
 //?  USING TWO POINTER METHOD
 //?  TIME && SPACE COMPLEXITY IS O(n)
 
-// function reverseNormal(str) {
-//     let arr=str.split('');
-//     let left=0;
-//     let right=arr.length-1;
+function reverseNormal(str) {
+    let arr=str.split('');
+    let left=0;
+    let right=arr.length-1;
     
-//     while(left<right) {
-//         let temp=arr[left];
-//         arr[left]=arr[right];
-//         arr[right]=temp;
-//         left++;
-//         right--;
-//     }
+    while(left<right) {
+        let temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        left++;
+        right--;
+    }
 
-//     return arr.join('');
-// }
+    return arr.join('');
+}
 
-// console.log(reverseNormal('malayalam'));
+console.log(reverseNormal('malayalam'));
 
 
 //?  USING RECURSIVE METHOD
 //?  TIME COMPLEXITY : O(n).
 //?  SPACE COMPLEXITY : O(n).
 
-// function reverRecusrion(str) {
-//     if(str.length <= 1) return str;
+function reverRecusrion(str) {
+    if(str.length <= 1) return str;
 
-//     return reverRecusrion(str.slice(1)) + str[0];
-// }
+    return reverRecusrion(str.slice(1)) + str[0];
+}
 
-// console.log(reverRecusrion('hello world'));
+console.log(reverRecusrion('hello world'));
 
 
 
 //? FUCTION TO REMOVE VOWELS 
-// function removeVowels(str) {
-//     let vowels = ['a','e','i','o','u'];
-//     for(let i=0;i<str.length;i++) {
-//         if(vowels.includes(str[i].toLowerCase())) {
-//             str = str.replace(str[i],'');
-//         }
-//     }
+function removeVowels(str) {
+    let vowels = ['a','e','i','o','u'];
+    for(let i=0;i<str.length;i++) {
+        if(vowels.includes(str[i].toLowerCase())) {
+            str = str.replace(str[i],'');
+        }
+    }
 
-//     return str;
-// }
+    return str;
+}
 
-// console.log(removeVowels('hello world'));
+console.log(removeVowels('hello world'));
 
 
 
-// function palindromeRecursive(str,s,e) {
-//     str = str.toLowerCase();
-//     if(s>=e) {
-//         return true;
-//     }
+function palindromeRecursive(str,s,e) {
+    str = str.toLowerCase();
+    if(s>=e) {
+        return true;
+    }
 
-//     if(str[s] !== str[e]) {
-//         return false;
-//     }
+    if(str[s] !== str[e]) {
+        return false;
+    }
 
-//     return palindromeRecursive(str,s+1,e-1);
-// }
+    return palindromeRecursive(str,s+1,e-1);
+}
 
-// const str = 'malayalam';
-// const s = 0;
-// const e = str.length-1;
-// console.log(palindromeRecursive(str,s,e));
+const str = 'malayalam';
+const s = 0;
+const e = str.length-1;
+console.log(palindromeRecursive(str,s,e));
 
 
 
