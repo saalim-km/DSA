18f70c0552bcd3f7a0462041cc6d51ecb21418e7
saalim-km <saalimkm@gmail.com>
2024-12-10 14:28:01 +0530

    added mergesort

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -1,1 +1,28 @@
-//? soon it will be here (give me some time to study :)
+//? MERGE SORT
+//? Time Complexity                             Space Complexity
+//? Divide Step : O(logn) , Merge Step : O(n)  ,      O(n)
+
+
+function mergeSort(arr) {
+    if(arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+
+    return merge(left,right);
+}
+
+function merge(left,right) {
+    const sortedArray = [];
+    while(left.length && right.length) {
+        if(left[0] < right[0]) {
+            sortedArray.push(left.shift());
+        }else {
+            sortedArray.push(right.shift());
+        }
+    }
+
+    return [...sortedArray,...left,...right];
+}
+console.log(mergeSort([5,4,3,2,1]));
