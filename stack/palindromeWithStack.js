9e2ed303e4c57cdbea951bbff48e11f3bc0dd951
saalim-km <saalimkm@gmail.com>
2024-12-15 14:59:14 +0530

    added palindrome using stack

---
+++ stack/palindromeWithStack.js
@@ -0,0 +1,16 @@
+function isPalindrome(str) {
+    let stack = [];
+
+    for(let char of str) {
+        stack.push(char);
+    }
+
+    for(let char of str) {
+        if(stack.pop() !== char) {
+            return false;
+        }
+    }
+
+    return true;
+}
+console.log(isPalindrome('madam'));
